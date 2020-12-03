ad2c1e76ee363b522d8de9d0113ac145b4bce1dd
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-12-03 06:15:49 +0400

    Coffee Machine

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,6 @@ import com.company.service.MenuService;
 public class Main {
 
     public static void main(String[] args) {
-//        UserService userService = new UserService();
-//        User user = new User("j",9);
-//
-//        UserService.userMenu();
-//        User u = UserService.userWhoCanDrink(UserService.createUser(UserService.readUserInfoFromFile()),"vaxo");
-//        System.out.println(u.getName());
-
         MenuService.createMenu();
     }
 }
